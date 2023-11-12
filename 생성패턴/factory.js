0d5a399db66c9a8ec240eebc6460bcf1d8374c6e
MinGyeom <87847136+reignkk1@users.noreply.github.com>
2023-11-12 17:46:08 +0900

    simple factory

--- 생성패턴/factory.js
+++ 생성패턴/factory.js
@@ -1,21 +1,45 @@
+// input 컴포넌트들을 각 패턴에 맞게 생성한다.
+
+class Button {}
+class Input {}
+const TextInput = class extends Input {
+  constructor() {
+    super();
+    console.log("create TextInput");
+  }
+};
+const RadioInput = class extends Input {
+  constructor() {
+    super();
+    console.log("create RadioInput");
+  }
+};
+const SubmitInput = class extends Input {
+  constructor() {
+    super();
+    console.log("create SubmitInput");
+  }
+};
+
 // simple factory
 
-class Todos extends Array {
-  get(todo) {
-    if (!this.includes(todo)) return console.log(todo + "not found");
-    return this[this.indexOf(todo)];
+class InputDerectory extends Array {
+  get(element) {
+    if (!this.includes(element)) return console.log("not found element");
+    return new this[this.indexOf(element)]();
   }
-  add(todo) {
-    if (!this.includes(todo)) return this.push(todo);
-  }
-  delete(todo) {
-    if (!this.includes(todo)) return console.log(todo + "not found");
-    return this.filter((todoItem) => todoItem !== todo);
+
+  add(element) {
+    if (element.__proto__ !== Input) return console.log("different type");
+    if (!this.includes(element)) return this.push(element);
   }
 }
 
-const todosArray = new Todos();
-todosArray.add("shopping");
-todosArray.add("work");
-todosArray.add("football");
-console.log(todosArray.get("work"));
+const inputDerectory = new InputDerectory();
+
+inputDerectory.add(TextInput);
+inputDerectory.add(RadioInput);
+inputDerectory.add(SubmitInput);
+inputDerectory.add(Button);
+
+console.log(inputDerectory.get(RadioInput));
